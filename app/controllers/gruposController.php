f0ae5c81ea1e2f53fdafd19690f2ceded7383967
GerardoBelard <belard86@gmail.com>
2022-04-25 15:58:00 -0500

    agregar grupos

--- app/controllers/gruposController.php
+++ app/controllers/gruposController.php
@@ -1,18 +1,35 @@
 <?php
+use \Verot\Upload\Upload;
 
+/**
+ * Plantilla general de controladores
+ * Versión 1.0.2
+ *
+ * Controlador de grupos
+ */
 class gruposController extends Controller {
+  private $id  = null;
+  private $rol = null;
+
   function __construct()
   {
-  //Validacion de inicio de sesion
+    // Validación de sesión de usuario, descomentar si requerida
     if (!Auth::validate()) {
       Flasher::new('Debes iniciar sesión primero.', 'danger');
       Redirect::to('login');
     }
-    
+
+    $this->id  = get_user('id');
+    $this->rol = get_user_role();
   }
   
   function index()
   {
+    if (!is_admin($this->rol)) {
+      Flasher::new(get_notificaciones(), 'danger');
+      Redirect::back();
+    }
+    
     $data = 
     [
       'title'  => 'Todos los Grupos',
@@ -27,12 +44,33 @@ class gruposController extends Controller {
 
   function ver($id)
   {
-    View::render('ver');
+    if (!is_admin($this->rol)) {
+      Flasher::new(get_notificaciones(), 'danger');
+      Redirect::back();
+    }
+
+    if (!$grupo = grupoModel::by_id($id)) {
+      Flasher::new('No existe el grupo en la base de datos.', 'danger');
+      Redirect::back();
+    }
+
+    $data =
+    [
+      'title'  => sprintf('Grupo %s', $grupo['nombre']),
+      'slug'   => 'grupos',
+      'button' => ['url' => 'grupos', 'text' => '<i class="fas fa-table"></i> Todos los grupos'],
+      'g'      => $grupo
+    ];
+
+    View::render('ver', $data);
   }
 
   function agregar()
   {
-   
+    if (!is_admin($this->rol)) {
+      Flasher::new(get_notificaciones(), 'danger');
+      Redirect::back();
+    }
 
     $data =
     [
@@ -46,21 +84,51 @@ class gruposController extends Controller {
 
   function post_agregar()
   {
+    try {
+      if (!check_posted_data(['csrf','nombre','descripcion'], $_POST) || !Csrf::validate($_POST['csrf'])) {
+        throw new Exception(get_notificaciones());
+      }
 
+      // Validar rol
+      if (!is_admin($this->rol)) {
+        throw new Exception(get_notificaciones(1));
+      }
+
+      $nombre      = clean($_POST["nombre"]);
+      $descripcion = clean($_POST["descripcion"]);
+
+
+      // Validar que el nombre del grupo no exista en la base de datos
+      $sql = 'SELECT * FROM grupos WHERE nombre = :nombre LIMIT 1';
+      if (grupoModel::query($sql, ['nombre' => $nombre])) {
+        throw new Exception(sprintf('Ya existe el grupo <b>%s</b> en la base de datos.', $nombre));
+      }
+
+      $data =
+      [
+        'numero'      => null,
+        'nombre'      => $nombre,
+        'descripcion' => $descripcion,
+        'horario'     => null,
+        'creado'      => now()
+      ];
+
+      // Insertar a la base de datos
+      if (!$id = grupoModel::add(grupoModel::$t1, $data)) {
+        throw new Exception(get_notificaciones(2));
+      }
+
+      Flasher::new(sprintf('Nuevo grupo <b>%s</b> agregado con éxito.', $nombre), 'success');
+      Redirect::back();
+
+    } catch (PDOException $e) {
+      Flasher::new($e->getMessage(), 'danger');
+      Redirect::back();
+    } catch (Exception $e) {
+      Flasher::new($e->getMessage(), 'danger');
+      Redirect::back();
+    }
   }
 
-  function editar($id)
-  {
-    View::render('editar');
-  }
-
-  function post_editar()
-  {
-
-  }
-
-  function borrar($id)
-  {
-    // Proceso de borrado
-  }
+  
 }
